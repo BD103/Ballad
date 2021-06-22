18557300c41eb3f10765825cdc79d8050a54f072
BD103 <>
2021-06-22 21:31:38 +0000

    chore: remove utils subgroup

--- src/ballad/__main__.py
+++ src/ballad/__main__.py
@@ -1,10 +1,19 @@
 import click
 
+from . import utils as _utils
+
 
 @click.group()
 def cli():
   """Install Poetry Project without Poetry"""
   pass
+
+
+@cli.command()
+@click.option("-l", "--lock", default="poetry.lock")
+@click.option("-o", "--output", default="poetry.json")
+def convert_lock(lock: str = "poetry.lock", output: str = "poetry.json"):
+  _utils.convert_lock(lock, output)
 
 
 if __name__ == "__main__":
