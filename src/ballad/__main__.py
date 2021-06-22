df58dae2995ce9b8fed2ce36de8997b99dd8efc5
BD103 <>
2021-06-22 22:40:54 +0000

    feat: add install command

--- src/ballad/__main__.py
+++ src/ballad/__main__.py
@@ -1,6 +1,6 @@
 import click
 
-from . import utils as _utils
+from . import core, utils
 
 
 @click.group()
@@ -10,10 +10,26 @@ def cli():
 
 
 @cli.command()
+@click.option("-d/-p", "--development/--production", default=True)
+@click.option("-l", "--lockfile", default="poetry.lock")
+@click.option("-q", "--quiet", is_flag=True)
+def install(development: bool, lockfile: str, quiet: bool):
+  """Install dependencies from lockfile using PIP"""
+  ballad = core.Ballad()
+  ballad.lockfile_name = lockfile
+
+  if not development:
+    ballad.environment = "production"
+
+  ballad.install(quiet)
+
+
+@cli.command()
 @click.option("-l", "--lock", default="poetry.lock")
 @click.option("-o", "--output", default="poetry.json")
-def convert_lock(lock: str = "poetry.lock", output: str = "poetry.json"):
-  _utils.convert_lock(lock, output)
+def convert_lock(lock: str, output: str):
+  """Reads lockfile, convert it to JSON, then outputs to file"""
+  utils.convert_lock(lock, output)
 
 
 if __name__ == "__main__":
